c87cb5adcb7b0b0d32741351343e58b018c7b4bc
jcjfgnl9205 <jcjfgnl9205@gmail.com>
2022-02-09 23:23:49 +0900

    Notice comment 更新フォーム修正

--- src/components/notice/Comments.js
+++ src/components/notice/Comments.js
@@ -3,6 +3,7 @@ import CommentAllow from './CommentAllow';
 
 const Comments = (props) => {
   const [ updateFlg, setUpdateFlg ] = useState(false);
+  const [ id, setId ] = useState(null);
   const [ msg, setMsg ] = useState(null)
   const textareaValue = useRef();
 
@@ -33,13 +34,13 @@ const Comments = (props) => {
                     <div>
                       { props.user && props.user.id === data.owner_id
                         ?
-                          updateFlg
+                          updateFlg && data.id === id
                           ? <span>
                               <i className="bi bi-pencil-fill m-1 update-btn" onClick={ () => update(textareaValue.current, data.id) }></i>
                               <i className="bi bi-x-lg m-1 update-cancel-btn" onClick={ () => { setUpdateFlg(false); setMsg(''); } }></i>
                             </span>
                           : <span>
-                              <i className="bi bi-pencil m-1 update-btn" onClick={ () => setUpdateFlg(true) }></i>
+                              <i className="bi bi-pencil m-1 update-btn" onClick={ () => { setUpdateFlg(true); setId(data.id); } }></i>
                               <i className="bi bi-trash m-1 delete-btn" onClick={ () => props.delete(data.id) }></i>
                             </span>
                         : <span></span>
@@ -48,7 +49,7 @@ const Comments = (props) => {
                     </div>
                   </div>
                   {
-                    updateFlg
+                    updateFlg && data.id === id
                     ? <CommentAllow comment={ data.comment } textareaRef={ textareaValue } msg={ msg } setMsg={ setMsg } />
                     : <div className="comment-comment" >
                       { 
